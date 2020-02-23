4a4094e618375b57ec61baa0d516a2490ef296ac
DavidMac94 <60328222+DavidMac94@users.noreply.github.com>
2020-02-23 22:45:37 +0000

    Update Wimbledon_classes.py

--- Wimbledon_classes.py
+++ Wimbledon_classes.py
@@ -365,12 +365,15 @@ class Board:
 
     def update(self, setsA, setsB, gamesA, gamesB, pointsA, pointsB):
         "Updates board, converting points to tennis equivalent if in a game (i.e. 1=15, 2=30....)"
+        tennis_points = ["0", "15", "30", "40", "A"]  
         self.SetsA.setText(setsA)
         self.SetsB.setText(setsB)
         self.GamesA.setText(gamesA)
         self.GamesB.setText(gamesB)
-        tennis_points = ["0", "15", "30", "40", "A"]            
-        if (gamesA == 6 and gamesB == 6) or (gamesA == 6 and gamesB == 7) or (gamesA == 7 and gamesA == 6) and (pointsA == "" and pointsB == ""):
+        if gamesA == "" and gamesB == "":
+            self.PointsA.setText(pointsA)
+            self.PointsB.setText(pointsB)
+        elif (gamesA == 6 and gamesB == 6) or (gamesA == 6 and gamesB == 7) or (gamesA == 7 and gamesA == 6) and (pointsA == "" and pointsB == ""):
             self.PointsA.setText(pointsA)
             self.PointsB.setText(pointsB)
         else:
